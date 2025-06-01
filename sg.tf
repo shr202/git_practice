7ca5793212727a72a9ba5362ba8f4386f43d7a50
shr202 <shruthib412@gmail.com>
2025-06-01 15:16:23 +0530

    changes remaing for vpc

---
+++ sg.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "Sg-Terra" {
+  name        = "allow_Terra"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.Vpc-Terra.id
+  ingress {
+    from_port   = 0
+    to_port  = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 0
+    to_port  = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Sg-Terra"
+  }
+}
